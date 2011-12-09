863e95181e26ad2ececb317903810164a84ff72f
unknown <sbeauge@Anchorage.strasbourg.astek.fr>
2011-12-09 22:16:16 +0100

    P11 ok Added Grid2d class

---
+++ Src/ProjectEuler/Lib/Grid2d.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lib
+{
+    public class Grid2d
+    {
+        private int[,] m_Values;
+
+        public static Grid2d Parse(string content)
+        {
+            var lines = content.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var values = lines.Select(l=>l.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)).ToList();
+
+            var result = new Grid2d();
+
+            result.m_Values = new int[values.Count(), values.First().Count()];
+
+            for (int i = 0; i < values.Count(); i++)
+            {
+                for (int j = 0; j < values.First().Count(); j++)
+                {
+                    result.m_Values[i, j] = int.Parse(values[i][j]);
+
+                }
+            }
+            return result;
+        }
+
+
+        public int Width { get { return m_Values.GetLength(0); } }
+        public int Height { get { return m_Values.GetLength(1); } }
+
+        public int this[int x, int y]
+        {
+            get { return m_Values[x, y]; }
+        }
+    }
+}
--- Src/ProjectEuler/P011/P11.cs
+++ Src/ProjectEuler/P011/P11.cs
@@ -2,13 +2,92 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Lib;
 
 namespace P011
 {
     class P11
     {
+        private const int PathLength = 4;
         static void Main(string[] args)
         {
+            var grid = Grid2d.Parse(Properties.Resources.GridP11);
+
+            var allPaths = new IEnumerable<int[]>[]{
+                GetHorizontalPaths(grid),
+                GetVerticalPaths(grid),
+                GetDiagonalNESWPaths(grid),
+                GetDiagonalNWSEPaths(grid)
+            }.SelectMany(e => e);
+
+            var result = allPaths.Select(
+                path=>path[0]*path[1]*path[2]*path[3]
+                ).Max();
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        private static IEnumerable<int[]> GetHorizontalPaths(Grid2d grid)
+        {
+            for (int x = 0; x < grid.Width - PathLength + 1; x++)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+                    yield return new int[PathLength] {
+                        grid[x,y],
+                        grid[x+1,y],
+                        grid[x+2,y],
+                        grid[x+3,y]
+                    };
+                }
+            }
+        }
+        private static IEnumerable<int[]> GetVerticalPaths(Grid2d grid)
+        {
+            for (int x = 0; x < grid.Width; x++)
+            {
+                for (int y = 0; y < grid.Height - PathLength + 1; y++)
+                {
+                    yield return new int[PathLength] {
+                        grid[x,y],
+                        grid[x,y+1],
+                        grid[x,y+2],
+                        grid[x,y+3]
+                    };
+                }
+            }
+        }
+        private static IEnumerable<int[]> GetDiagonalNWSEPaths(Grid2d grid)
+        {
+            for (int x = 0; x < grid.Width - PathLength + 1; x++)
+            {
+                for (int y = 0; y < grid.Height - PathLength + 1; y++)
+                {
+                    yield return new int[PathLength] {
+                        grid[x,y],
+                        grid[x+1,y+1],
+                        grid[x+2,y+2],
+                        grid[x+3,y+3]
+                    };
+                }
+            }
+        }
+        private static IEnumerable<int[]> GetDiagonalNESWPaths(Grid2d grid)
+        {
+            for (int x = 0; x < grid.Width - PathLength + 1; x++)
+            {
+                for (int y = PathLength-1; y < grid.Height; y++)
+                {
+                    yield return new int[PathLength] {
+                        grid[x,y],
+                        grid[x+1,y-1],
+                        grid[x+2,y-2],
+                        grid[x+3,y-3]
+                    };
+                }
+            }
         }
     }
 }
