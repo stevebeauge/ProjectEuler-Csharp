48c2ed505d6574a360eb2d498c22b05a6f5a3782
Steve Beaugé <steve_beauge@msn.com>
2011-12-12 15:13:02 +0100

    Added P19 and P20
    Downloaded a copy of the problem statements as the web site is very very very slow

---
+++ Src/ProjectEuler/P020/P20.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lib;
+
+namespace P020
+{
+    class P20
+    {
+        static void Main(string[] args)
+        {
+
+            var result = 100.Factorial().ToString().Select(c => c - '0').Sum();
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+        }
+    }
+}
