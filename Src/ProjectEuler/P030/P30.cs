3e9968d2465d1e50b5631c657caa864d17d8bc77
Steve Beaugé <steve_beauge@msn.com>
2011-12-16 13:48:43 +0100

    Updated algorithm for P30

--- Src/ProjectEuler/P030/P30.cs
+++ Src/ProjectEuler/P030/P30.cs
@@ -9,17 +9,16 @@ namespace P030
     {
         static void Main(string[] args)
         {
-            foreach (var nbr in GetMatchingNumbers(5))
-            {
-                Console.WriteLine(nbr);
-            }
-            Console.WriteLine(GetMatchingNumbers(5).Sum());
+
+            Console.WriteLine("   " + GetMatchingNumbers(4).Sum());
+
+            Console.WriteLine("   " + GetMatchingNumbers(5).Sum());
             Console.ReadLine();
         }
 
         static IEnumerable<int> GetMatchingNumbers(int power)
         {
-            for (int i = 2; i < 100000; i++)
+            for (int i = 2; i <(power + 1)*(Math.Pow(9,power)); i++)
             {
                 var sumOfPowers = 0;
                 var tempi = i;
@@ -28,7 +27,11 @@ namespace P030
                     sumOfPowers += (int)Math.Pow( tempi % 10, power);
                     tempi /= 10;
                 }
-                if(sumOfPowers == i) yield return i;
+                if (sumOfPowers == i)
+                {
+                    yield return i;
+                    Console.WriteLine("With Power {0}, {1} matches", power, i);
+                }
             }
         }
     }
