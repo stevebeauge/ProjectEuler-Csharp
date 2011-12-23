3fa749f3c4dda8dede6e2766e9bad6fe9699e846
Steve B <steve_beauge@msn.com>
2011-12-23 22:34:41 +0100

    Solved P24

---
+++ Src/ProjectEuler/P024/P24.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lib;
+
+namespace P024
+{
+    class P24
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            var target = 1000000UL - 1;
+            var digits = "0123456789";
+
+            for (int i = 9; i >= 1; i--) 
+            {
+                var iFact = i.FactorialSmall();
+                var index = target / iFact;
+                target = target % iFact;
+                var digit = digits[(int)index];
+                sb.Append(digit);
+                digits = digits.Replace(digit.ToString(), "");
+            }
+            sb.Append(digits); // Add the last remaining digit
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
